81d9084680a5b1a329f61945989e80ecae4f5add
GaelScript <gaelmota@dusty.tamiu.edu>
2024-09-24 14:16:18 -0500

    Added basic shell prefix test; added gitignore for testing folder.

--- JS-DOS.h
+++ JS-DOS.h
@@ -1,6 +1,9 @@
 #ifndef JS_DOS_H
 #define JS_DOS_H
 
-void JS_DOS_Interface();
+void JS_DOS_Interface(){
+    
+    printf("C:\\> ");
+}
 
 #endif // JS_DOS_H
--- JayNIX.h
+++ JayNIX.h
@@ -1,7 +1,10 @@
 #ifndef JayNIX_H
 #define JayNIX_H
 
-void JayNIX_Interface();
+void JayNIX_Interface(){
+    
+    printf("Jsh > ");
+}
 
 
 #endif // JayNIX_H
