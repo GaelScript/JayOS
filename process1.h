b0343a960dd9dbd2882f2b3e30721e20fd4a90df
Lesli Mohamed <leslymohamed7676@gmail.com>
2024-09-28 17:59:08 -0500

    Create process1.h
    Process 1 creation and termination simulation.

---
+++ process1.h
@@ -0,0 +1,70 @@
+#ifndef process1_h
+#define process1_h
+
+#include <stdio.h> 
+#include <unistd.h> //Added for sleep() function to work
+
+struct ProcessControlBlock{
+    int processID;
+    int memoryAllocated;
+};
+
+//Process simulation: I/O
+void inputOutputProcess(){
+    struct ProcessControlBlock pcb;
+    pcb.processID = 1;
+    pcb.memoryAllocated = 120;
+
+    //First, we create process and allocate memory for it
+    printf("Process is being created with PID: %d", pcb.processID);
+        for (int i = 0; i < 3; ++i) {
+        printf(".");
+        fflush(stdout);
+        sleep(1);
+    }
+    printf("\n");
+    printf("%d KB of memory has been allocated for the process.\n", pcb.memoryAllocated);
+    printf("Process Control Block is initializing, and we are setting up registers");
+        for (int i = 0; i < 3; ++i) {
+        printf(".");
+        fflush(stdout);
+        sleep(1);
+    }
+    printf("\n\n");
+
+    //Process simulation occurs here
+    int number;
+    printf("Input/Output process has begun.\n");
+
+    printf("Please enter a number:\n");
+    scanf("%d", &number);
+
+    printf("%d squared is equal to %d.\n", number, number * number);
+
+    printf("Input/Output process has finished!\n\n");
+
+    printf("Terminating process..\n");
+    sleep(1);
+
+    //Releasing memory of process and cleaning up
+    int memoryDeAllocated = 120;
+    printf("Releasing %d KB of memory", memoryDeAllocated);
+    for (int i = 0; i < 3; ++i) {
+        printf(".");
+        fflush(stdout);
+        sleep(1);
+    }
+    printf("\n");
+
+    printf("Cleaning up Process Control Block and resetting registers");
+    for (int i = 0; i < 4; ++i) {
+        printf(".");
+        fflush(stdout);
+        sleep(1);
+    }
+    printf("\n");
+    printf("Process has terminated successfully!\n");
+}
+
+
+#endif
